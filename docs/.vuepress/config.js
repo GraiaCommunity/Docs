76a44b54632abdae916a187bd204abb6d0a225f5
I Love Study <1450069615@qq.com>
2021-12-11 13:24:21 +0800

    Add tutorial of making ero bot

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,20 @@ module.exports = {
   description: "GraiaX社区文档",
 
   themeConfig: {
-    logo: "https://vuejs.org/images/logo.png"
+    logo: "https://vuejs.org/images/logo.png",
+    sidebar: {
+      "/make_ero_bot/": [
+        {
+          text: "手把手教你写机器人",
+          children: [
+            "/make_ero_bot/README.md",
+            "/make_ero_bot/1_hello_ero.md",
+            "/make_ero_bot/2_bot_start.md",
+            "/make_ero_bot/3_ero_comes.md"
+          ]
+        }
+      ]
+    }
   },
   plugins: [
     [
@@ -15,5 +28,5 @@ module.exports = {
         componentsDir: path.resolve(__dirname, "./components")
       }
     ]
-  ]
+  ],
 };
