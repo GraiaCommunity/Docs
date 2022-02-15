937f8e0e5d6498ee28bffe64a33cc09ddd75a9b3
Redlnn <w731347477@gmail.com>
2022-02-15 19:06:30 +0800

    新的音量条设计

--- docs/.vuepress/components/AudioBar.vue
+++ docs/.vuepress/components/AudioBar.vue
@@ -1,27 +1,56 @@
 <template>
-  <input
-    id="range"
-    type="range"
-    min="0"
-    max="100"
-    value="100"
-    oninput='
-      var range = document.getElementById("range");
-      range.style.backgroundSize = `${range.value}%, 100%`;
-      for (let i of document.getElementsByTagName("audio")) {
-        i.volume = range.value / 100;
-      };
-      document.getElementById("volume").innerHTML = `${value}%`;'
-  />
-  <p id="volume" style="display: inline; margin-left: 10px">100%</p>
+  <span id="volume">
+    <div class="volume">
+      <input
+        id="range"
+        type="range"
+        min="0"
+        max="100"
+        value="100"
+        oninput='
+          var range = document.getElementById("range");
+          range.style.backgroundSize = `${range.value}%, 100%`;
+          for (let i of document.getElementsByTagName("audio")) {
+            i.volume = range.value / 100;
+          };
+          document.getElementById("volume-num").innerHTML = `${value}%`;'
+      />
+      <p id="volume-num">100%</p>
+    </div>
+  </span>
 </template>
 
 <style scoped>
+#volume {
+  display: inline-block;
+  vertical-align: middle;
+  width: 50%;
+  height: 1rem;
+}
+#volume-num {
+  padding: .1rem .3rem;
+  margin-left: 4px;
+  width: 46px;
+  font-size: .9rem;
+  text-align: right;
+  background: var(--c-bg-lighter);
+  border-radius: 5px;
+}
+.volume {
+  display: flex;
+  height: 1rem;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+}
 input[type="range"] {
   -webkit-appearance: none;
-  width: 50%;
+  width: 100%;
   border-radius: 10px; /*这个属性设置使填充进度条时的图形为圆角*/
   background: linear-gradient(var(--c-text), var(--c-text)) no-repeat;
+  background-color: var(--c-bg-arrow);
   background-size: 100% 100%;
 }
 input[type="range"]::-webkit-slider-thumb {
@@ -29,32 +58,31 @@ input[type="range"]::-webkit-slider-thumb {
 }
 /*轨道设计*/
 input[type="range"]::-webkit-slider-runnable-track {
-  height: 15px;
+  height: 5px;
   border-radius: 10px; /*将轨道设为圆角的*/
-  box-shadow: 0 1px 1px #def3f8, inset 0 0.125em 0.125em #0d1112;
+  padding-bottom: .3rem;
 }
 input[type="range"]::-moz-range-track {
-  height: 17px;
+  height: 5px;
   border-radius: 10px; /*将轨道设为圆角的*/
-  box-shadow: 0 1px 1px #def3f8, inset 0 0.125em 0.125em #0d1112;
 }
-/*删除内框*/
+/* 删除内框*/
 input[type="range"]:focus {
   outline: none;
 }
 /*滑块*/
 input[type="range"]::-webkit-slider-thumb {
   -webkit-appearance: none;
-  height: 25px;
-  width: 25px;
-  margin-top: -5px; /*使滑块超出轨道部分的偏移量相等*/
+  height: 1rem;
+  width: 1rem;
+  margin-top: -.3rem; /*使滑块超出轨道部分的偏移量相等*/
   background: var(--c-text);
   border-radius: 50%; /*外观设置为圆形*/
 }
 input[type="range"]::-moz-range-thumb {
-  height: 25px;
-  width: 25px;
-  margin-top: -5px; /*使滑块超出轨道部分的偏移量相等*/
+  height: 1rem;
+  width: 1rem;
+  margin-top: -.3rem; /*使滑块超出轨道部分的偏移量相等*/
   background: var(--c-text);
   border-radius: 50%; /*外观设置为圆形*/
 }
