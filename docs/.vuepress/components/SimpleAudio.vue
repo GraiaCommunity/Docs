484b40f4fe90ba17b4d8e4763a2377cbe1ba45b1
zzzzz167 <2742400566@qq.com>
2022-02-16 20:20:48 +0800

    播放器动态效果优化

--- docs/.vuepress/components/SimpleAudio.vue
+++ docs/.vuepress/components/SimpleAudio.vue
@@ -61,7 +61,7 @@ export default {
 }
 
 .player-circle {
-  border: 3px solid #999999;
+  border: 2px solid #999999;
   border-radius: 50%;
   position: relative;
 }
@@ -71,21 +71,20 @@ export default {
   top: 13px;
   width: 5px;
   height: 5px;
-  background: #cccccc;
 }
 
 .player-circle.second {
   width: 25px;
   height: 25px;
   left: 7px;
-  top: -5px;
+  top: -2px;
 }
 
 .player-circle.third {
   width: 40px;
   height: 40px;
-  top: -42px;
-  left: 1px;
+  top: -37px;
+  left: 2px;
 }
 
 .player-symbol.on-play .second{
