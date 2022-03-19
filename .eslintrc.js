83d46671909595bcd47383033775b4cf62968b32
Redlnn <w731347477@gmail.com>
2022-03-19 18:32:21 +0800

    Add configuration files for Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  extends: [
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      extends: 'vuepress',
+      rules: {
+        'vue/require-default-prop': 'off',
+        'no-undef': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx', '*.vue'],
+      extends: 'vuepress-typescript',
+      parserOptions: {
+        project: ['tsconfig.json'],
+      },
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        'no-undef': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      },
+    },
+    {
+      files: ['*.vue'],
+      globals: {
+        defineEmits: 'readonly',
+        defineProps: 'readonly',
+      },
+      rules: {
+        'vue/multi-word-component-names': 'off',
+        'vue/require-default-prop': 'off',
+        // disable for setup script
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
+      files: ['docs/**'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+}
