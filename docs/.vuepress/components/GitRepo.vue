128f18e93dd6195ea1476d3c0e26d2d344e00978
Redlnn <w731347477@gmail.com>
2022-03-24 15:36:30 +0800

    Add a new repo & Improve repo component
    - 仓库卡片组件不再只能指向 GitHub 了，可以自定义链接了
    - 限制了仓库卡片组件的标题及介绍的行数

--- docs/.vuepress/components/GitRepo.vue
+++ docs/.vuepress/components/GitRepo.vue
@@ -13,10 +13,15 @@
         >
           <path
             fill-rule="evenodd"
-            d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
+            d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0
+            01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75
+            0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356
+            0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0
+            00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25
+            0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
           />
         </svg>
-        <a :href="`https://github.com/${user}/${repo}`">{{ user }}/{{ repo }}</a>
+        <a :href="href ? href : `https://github.com/${user}/${repo}`">{{ user }}/{{ repo }}</a>
       </div>
       <p class="repo-description">&#9889; <slot>No description here!</slot></p>
       <p class="repo-meta">
@@ -33,10 +38,11 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'GithubRepo',
+  name: 'GitRepo',
   props: {
     user: String,
     repo: String,
+    href: String,
   },
 })
 </script>
@@ -45,15 +51,16 @@ export default defineComponent({
 .repo {
   display: inline-block;
   width: 350px;
+  height: 130px;
   margin: 0.5em;
   padding: 16px;
   /* border: 1px solid #d0d7de; */
   border-radius: 6px;
   background-color: var(--c-tip-bg);
 }
-@media (max-width: 959px) {
+@media (max-width: 1140px) {
   .repo {
-    max-width: calc(100% - 2rem);
+    max-width: calc(100% - 1rem);
   }
 }
 @media (max-width: 419px) {
@@ -64,6 +71,7 @@ export default defineComponent({
 .repo-content {
   display: flex;
   flex-direction: column;
+  height: 100%;
 }
 .repo-svg {
   display: inline-block;
@@ -72,11 +80,15 @@ export default defineComponent({
   fill: currentColor;
 }
 .repo-title {
-  display: flex;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
   align-items: flex-end;
+  color: var(--c-tip);
 }
 .repo-title > svg {
   margin-right: 4px;
+  color: var(--c-text);
 }
 .repo-title > a {
   word-break: break-word;
@@ -85,12 +97,17 @@ export default defineComponent({
   color: var(--c-tip);
 }
 .repo-description {
+  display: -webkit-box; // 虽然是 webkit 但火狐也可以用
   flex: 1 0 auto;
   width: 100%;
   margin-top: 8px;
   margin-bottom: 0;
   font-size: 12px;
   color: var(--c-text-light);
+  overflow: hidden;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  text-overflow: ellipsis;
 }
 .repo-meta {
   margin-top: 8px;
