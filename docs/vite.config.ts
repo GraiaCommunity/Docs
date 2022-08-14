719c872dece4ca23088da7dbc67dee9a67752a75
Kaedehara Kazuha <w731347477@gmail.com>
2022-08-14 22:50:51 +0800

    Update vite.config.ts

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,7 +1,7 @@
 // 由于 Pnpm 和 VitePress 的关系，不可以手动安装 vite 依赖，否则会报错
 // import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
-import importToCDN from 'vite-plugin-cdn-import'
+import externalGlobals from 'rollup-plugin-external-globals'
 
 // https://cn.vitejs.dev/config/
 export default {
@@ -20,18 +20,14 @@ export default {
       algorithm: 'brotliCompress',
       ext: '.br',
     }),
-    importToCDN({
-      modules: [
-        {
-          name: 'mermaid',
-          var: 'mermaid',
-          path: 'https://cdn.bootcdn.net/ajax/libs/mermaid/9.1.5/mermaid.min.js'
-        }
-      ]
-    })
   ],
   server: {
     port: 3000,
     host: '0.0.0.0',
+  },
+  build: {
+    rollupOptions: {
+      plugins: [externalGlobals({ mermaid: 'mermaid' })],
+    },
   },
 }
