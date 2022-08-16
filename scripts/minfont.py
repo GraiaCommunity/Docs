9b7697ff8b73d806c83e6f7074fcae62c553d92a
Redlnn <w731347477@gmail.com>
2022-08-16 23:18:40 +0800

    [ci skip] remove unuse dep & perfect license

--- scripts/minfont.py
+++ scripts/minfont.py
@@ -3,10 +3,34 @@
 
 """
 minfont.js 的 Python 版，速度没有 minfont.js 的快，但生成的字体文件大小更小
+
+MIT License
+
+Copyright (c) 2021 GraiaCommunity
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+
+@author Redlnn <w731347477@gmail.com>
+@website https://github.com/GraiaCommunity/Docs
 """
 
 import os
-import platform
 from os.path import basename, exists, join
 
 from fontTools import subset
