11e68ac0fe2e1d0d24f8a9d1191590e345e5123d
Redlnn <w731347477@gmail.com>
2022-08-30 12:14:32 +0800

    更新依赖 & 格式化代码

--- types/shims.d.ts
+++ types/shims.d.ts
@@ -1,12 +1,12 @@
 declare module 'rollup-plugin-external-globals'
 declare module '*.vue' {
-    import type { ComponentOptions } from 'vue'
-    const Component: ComponentOptions
-    export default Component
-  }
+  import type { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
 
-  declare module '*.md' {
-    import type { ComponentOptions } from 'vue'
-    const Component: ComponentOptions
-    export default Component
-  }
+declare module '*.md' {
+  import type { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
