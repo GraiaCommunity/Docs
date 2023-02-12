ff793f7c93650a4fa6acb70680cdeb201c8315a8
Redlnn <w731347477@gmail.com>
2023-02-12 23:11:15 +0800

    :sparkles: 支持脚注

--- docs/.vitepress/mdEnhance/index.ts
+++ docs/.vitepress/mdEnhance/index.ts
@@ -1,4 +1,5 @@
 import MarkdownIt from 'markdown-it'
+import footnote_plugin from 'markdown-it-footnote'
 
 import { containerPlugin } from './cunstomContainrt'
 import { mermaidPlugin } from './mermaid'
@@ -8,4 +9,5 @@ export default (md: MarkdownIt) => {
   md.use(containerPlugin)
   md.use(mermaidPlugin)
   md.use(tasklistPlugin)
+  md.use(footnote_plugin)
 }
